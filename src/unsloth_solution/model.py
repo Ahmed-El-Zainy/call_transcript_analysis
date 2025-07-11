cdc6f4e6fa34bbd73a64cea2aa5d0c3e7429f2d3
Ahmed Mustafa <Ahmed_Mustafa_1251@fci.helwan.edu.eg>
2025-07-11 18:07:26 +0300

    update the code

--- src/unsloth_solution/model.py
+++ src/unsloth_solution/model.py
@@ -1,30 +1,24 @@
 from unsloth import FastModel
 import torch
+import yaml
 
-fourbit_models = [
-    # 4bit dynamic quants for superior accuracy and low memory use
-    "unsloth/gemma-3n-E4B-it-unsloth-bnb-4bit",
-    "unsloth/gemma-3n-E2B-it-unsloth-bnb-4bit",
-    # Pretrained models
-    "unsloth/gemma-3n-E4B-unsloth-bnb-4bit",
-    "unsloth/gemma-3n-E2B-unsloth-bnb-4bit",
+with open('config_unsloth.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
-    # Other Gemma 3 quants
-    "unsloth/gemma-3-1b-it-unsloth-bnb-4bit",
-    "unsloth/gemma-3-4b-it-unsloth-bnb-4bit",
-    "unsloth/gemma-3-12b-it-unsloth-bnb-4bit",
-    "unsloth/gemma-3-27b-it-unsloth-bnb-4bit",
-] # More models at https://huggingface.co/unsloth
+
+
+
 
 
 
 def init_model():
     model, tokenizer = FastModel.from_pretrained(
-        model_name = "unsloth/gemma-3n-E4B-it",
-        dtype = None, # None for auto detection
-        max_seq_length = 1024, # Choose any for long context!
-        load_in_4bit = True,  # 4 bit quantization to reduce memory
-        full_finetuning = False, # [NEW!] We have full finetuning now!
+        model_name =config["model"]["model_name"],
+        dtype = config["model"]["dtype"], # None for auto detection
+        max_seq_length = config["model"]["max_seq_length"], # Choose any for long context!
+        load_in_4bit = config["model"]["load_in_4bit"],  # 4 bit quantization to reduce memory
+        full_finetuning = config["model"]["full_finetuning"], # [NEW!] We have full finetuning now!
+        token = config["model"]["token"], # use one if using gated models
     )
     return model, tokenizer
 
